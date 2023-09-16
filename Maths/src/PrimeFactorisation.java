50938d8506601490761de01cc8e787d8fffb119a
NishchayAnand <nishchay.anand63@gmail.com>
2023-09-16 09:44:26 +0530

    prime factorization

--- Maths/src/PrimeFactorisation.java
+++ Maths/src/PrimeFactorisation.java
@@ -1,41 +1,65 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrimeFactorisation {
 	
+	private static List<Integer> uniquePrimeFactorsBruteForce(int N){
+        
+        List<Integer> arr = new ArrayList<>();
+        
+        int prime_factor = 2;
+        
+        while(N>1){
+            if(N%prime_factor==0){
+                arr.add(prime_factor);
+                while(N%prime_factor==0){
+                    N = N/prime_factor;
+                }
+            } else {
+                prime_factor++;
+            }
+        }
+        
+        return arr;
+    }
+    
+    private static List<Integer> uniquePrimeFactorsBruteForceRefactored(int N){
+        
+        List<Integer> arr = new ArrayList<>();
+        
+        for(int prime_factor = 2; prime_factor <=N; prime_factor++){
+            if(N%prime_factor==0){
+                arr.add(prime_factor);
+                while(N%prime_factor==0){
+                    N = N/prime_factor;
+                }
+            }
+        }
+        
+        return arr;
+    }
+    
+    private static List<Integer> uniquePrimeFactorsOptimized(int N){
+        
+        List<Integer> arr = new ArrayList<>();
+        
+        for(int pf = 2; pf*pf <=N; pf++){
+            if(N%pf==0){
+                arr.add(pf);
+                while(N%pf==0){
+                    N = N/pf;
+                }
+            }
+        }
+        
+        if(N>1) arr.add(N);
+        
+        return arr;
+    }
+    
 	public static void main(String args[]) {
 		
-		Scanner scn = new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		int num = scn.nextInt();
-		
-		System.out.print("Prime Factors: ");
-		
-		/* This code shows that the conditional variable of the for loop can be changed
-		for(int i=2; i<=num; i++) {
-			while(num%i==0) {
-				num/=i;
-				//System.out.print(i+" ");
-			}
-		}
-		*/
-		
-		// In case code, calculating time complexity will be difficult, i.e., can't be deduced 
-		// by just seeing the code  
-		int i = 2;
-		while(i*i<num) {
-			if(num%i==0) {
-				num/=i;
-				System.out.print(i+" ");
-			} else {
-				i++;
-			}
-		}
-		
-		if(num!=1) {
-			System.out.print(num);
-		}
-		
-		scn.close();
+		System.out.println(uniquePrimeFactorsOptimized(15));
 		
 	}
 
