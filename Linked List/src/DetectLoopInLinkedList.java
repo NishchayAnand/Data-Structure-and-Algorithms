0dee3e80f0382614516ea224b0680e1bb3bf0336
NishchayAnand <nishchay.anand63@gmail.com>
2023-11-08 17:45:49 +0530

    understand floyd cycle detection algorithm

---
+++ Linked List/src/DetectLoopInLinkedList.java
@@ -0,0 +1,40 @@
+
+/* Problem Statement: Given the head of a linked list, determine if the linked list has a cycle in it.
+ * 
+ * General Observations:
+ * 
+ * 	- Brute Force Approach: 
+ * 
+ * 		- use a hashMap(keys = Node, values = boolean) to mark the nodes as visited as we iterate over the
+ * 		  linked list.
+ * 
+ * 		NOTE: make sure to use key as Nodes to make sure the algorithm works fine for duplicate node 
+ * 			  values.
+ * 
+ *  	- Time Complexity Analysis: O(N).
+ *  
+ *  	- Space Complexity Analysis: O(N).
+ *  
+ *  - Floyd cycle detection:
+ *  
+ *  	- If you have two pointers on an infinite strip separated by a distance of k units, and you move 
+ *  	  the first pointer by 2 units and the second pointer by 1 unit in each iteration, the distance 
+ *  	  between the two pointers will decrease by 1 in each iteration.
+ *  	
+ *  	- Given that the distance between the pointers decreases by 1 unit with every iteration, 
+ *  	  eventually, the pointers will collide when the distance between them becomes 0.
+ *  
+ *  	- A cycle is like a infinite strip. 
+ * 	
+ * 
+ * */
+
+
+public class DetectLoopInLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Linked List/src/RemoveLoopInLinkedList.java
@@ -0,0 +1,24 @@
+
+/* Problem Statement: Given a linked list such that it may contain a loop. Remove the loop from the linked
+ *  				  list, if it is present, i.e. unlink the last node which is forming the loop.
+ *  
+ *  General Observations:
+ *  
+ *  	- 
+ *  
+ *  	- 
+ * 
+ * */
+
+public class RemoveLoopInLinkedList {
+	
+	public static void removeLoop(Node head) {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
