c1c16ff0cf8910269f2b62bf58d86e89a0acda9c
NishchayAnand <nishchay.anand63@gmail.com>
2024-02-14 10:56:27 +0530

    palindrome partitioning - recursion

---
+++ Recursions/src/PalindromePartitioning.java
@@ -0,0 +1,23 @@
+
+/* Problem Statment: Given a string 's', partition 's' such that every substring of the partition is a 
+ * 				     palindrome. Return all possible palindrome partitioning of s.
+ * 
+ * General Observations:
+ * 
+ * 	- For every nth letter in the given string 's', there can be 'n' substrings ending with the nth 
+ *    letter. Example, for s = 'aab',
+ *    	- substrings ending with the 1st letter ('a') -> 'a'
+ *    	- substrings ending with the 2nd letter ('a') -> 'aa', 'a'
+ *    	- substrings ending with the 3rd letter ('b') -> 'aab', 'ab', 'b'
+ * 
+ *  - 
+ * 
+ * */
+
+public class PalindromePartitioning {
+
+	public static void main(String[] args) {
+
+	}
+
+}
