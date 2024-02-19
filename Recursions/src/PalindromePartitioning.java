40cc05a7a0c4b676bf9829221322af37c709a600
NishchayAnand <nishchay.anand63@gmail.com>
2024-02-19 11:01:49 +0530

    understanding partitioning of a string

--- Recursions/src/PalindromePartitioning.java
+++ Recursions/src/PalindromePartitioning.java
@@ -4,13 +4,18 @@
  * 
  * General Observations:
  * 
+ *  - The task is "explore different ways of partitioning the given string into substrings" (visualize 
+ *    using recursion tree) such that "each substring is a palindrome". 
+ * 
  * 	- For every nth letter in the given string 's', there can be 'n' substrings ending with the nth 
  *    letter. Example, for s = 'aab',
  *    	- substrings ending with the 1st letter ('a') -> 'a'
  *    	- substrings ending with the 2nd letter ('a') -> 'aa', 'a'
  *    	- substrings ending with the 3rd letter ('b') -> 'aab', 'ab', 'b'
  * 
- *  - 
+ *  - The problem is naturally recursive in nature, i.e., we can explore all substrings ending with the 
+ *    letter under-consideration and trust the procedure to do the same with the remaining (un-explored)
+ *    letters of the input string. 
  * 
  * */
 
