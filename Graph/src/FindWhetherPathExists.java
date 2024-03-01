1a02262eeb8f797c3778bff51a23ab47b3fe91b2
NishchayAnand <nishchay.anand63@gmail.com>
2024-03-01 09:18:32 +0530

    done understanding has path problem

--- Graph/src/FindWhetherPathExists.java
+++ Graph/src/FindWhetherPathExists.java
@@ -5,8 +5,8 @@
  * 
  * 			- a cell with value = '1' represent the 'source' cell.
  * 			- a cell with value = '2' represent the 'destination' cell.
- * 			- cells with value = '3' represent cells which can be traversed.
- * 			- cells with value = '0' represent cells which can't be traversed.
+ * 			- cells with value = '3' represent 'paths' which can be traversed.
+ * 			- cells with value = '0' represent 'walls' through which we can't traverse.
  * 
  * 		- Check if there is a path possible from the 'source' cell to the 'destination' cell.
  *  
@@ -25,17 +25,78 @@
  * 		- Perform Depth-First-Search on the 'source' cell to check see if we can reach the 'destination' 
  * 		  cell.
  * 
- *  - Hypothesis:
- *  	
- *  	- F(arr, r, c, visited)
+ * 		- Time Complexity Analysis:
  * 
+ * 			- In worst-case scenario, we would need to traverse the entire input array of size n x n to
+ * 			  reach the destination. Hence, time complexity = O(n^2).
+ * 
+ * 		- Space Complexity Analysis:
+ * 
+ * 			- We need to maintain a 2-D boolean visited array of size n x n. Hence, space complexity = O(n^2). 
  * 
  * */
 
 public class FindWhetherPathExists {
+	
+	private static int[][] directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+	
+	private static boolean hasPath(int[][] grid, int r, int c, boolean[][] visited) {
+		
+		if(grid[r][c] == 2) {
+			return true;
+		}
+		
+		visited[r][c] = true;
+		
+		for(int[] dir: directions) {
+	
+			int nr = r+dir[0];
+			int nc = c+dir[1];
+			
+			boolean isValidCell = (nr>=0) && (nr<grid.length) && (nc>=0) && (nc<grid[0].length);
+			
+			if( isValidCell && grid[nr][nc] != 0 && !visited[nr][nc] ) {
+				
+				boolean pathExists = hasPath(grid, nr, nc, visited);
+				if(pathExists) return true;
+				
+			}
+			
+		}
+		
+		return false;
+		
+	}
 
 	public static void main(String[] args) {
 		
+		int[][] grid = { {3,0,3,0,0}, {3,0,0,0,3}, {3,3,3,3,3}, {0,2,3,3,0}, {3,0,0,1,3} };
+		
+		/* 
+		 * grid = [ [3 0 3 0 0],
+		 * 			[3 0 0 0 3],
+		 * 			[3 3 3 3 3],
+		 * 			[0 2 3 0 0],
+		 * 			[3 0 0 1 3] ]
+		 * 
+		 * */
+		
+		int sr = -1;
+		int sc = -1;
+		
+		for(int r=0; r<grid.length; r++) {
+			for(int c=0; c<grid[0].length; c++) {
+				if(grid[r][c] == 1) {
+					sr = r;
+					sc = c;
+					break;
+				}
+			}
+		}
+		
+		boolean[][] visited = new boolean[grid.length][grid[0].length];
+		
+		System.out.println("Path Exist: " + hasPath(grid, sr, sc, visited));
 
 	}
 
