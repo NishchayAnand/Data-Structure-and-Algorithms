47a7f4f6bb8e7525e65740b2b1456447353cd7cb
NishchayAnand <nishchay.anand63@gmail.com>
2024-03-01 11:39:37 +0530

    still understanding pacific atlantic water flow

--- Graph/src/PacificAtlanticWaterFlow.java
+++ Graph/src/PacificAtlanticWaterFlow.java
@@ -2,22 +2,50 @@
 /* Problem Statement: 
  * 
  * 	- Given an m x n input array where:
+ * 
  * 		 - "left" and "top" edges mark the Pacific Ocean
  * 		 - "right" and "bottom" edges mark the Atlantic Ocean. 
  * 
  * 	- Each cell in the input array represent is a piece of island. The value in each cell denotes the 
  * 	  height of that piece of land above sea level.
  * 
- * 	- Rain water can flow to neighboring cells directly {"up", "bottom", "right", "left"} if the 
- *    neighboring cell's value is less than or equal to the current cell's value. 
+ * 	- From a given 'cell', rain water can flow to its neighboring cells (i.e., cells "up", "below", 
+ *    "right" and "left" of the current 'cell') if:
+ *    		- "neighboring cell's value" <= "current cell's value". 
  * 
- * 	- Return a 2-D list of coordinates from where rain water can flow to both the Pacific and Atlantic 
- *    oceans.
+ * 	- Return a 2-D list of coordinates from where rain water can flow, i.e., "connected" to both the 
+ * 	  Pacific and Atlantic Oceans.
  * 
  * General Observations:
+ * 
+ * 	- Approach 1:
+ * 
+ * 		- For any 'cell':
+ *  		- use DFS to check if any of its neighbour is connected to both the Pacific and Atlantic 
+ *  		  Oceans, 
+ *  		- then the 'cell' is connected to both the Pacific and Atlantic Oceans.
+ *  
+ *  - Approach 2 (think of it as a Graph problem):
+ * 
+ * 		- All cells on the "left" and "top" edge are connected to the Pacific Ocean.
+ * 
+ * 		- All cells on the "right" and "bottom" edge are connected to the Atlantic Ocean. 
  *    
- *  - Water can flow from a 'cell' to both Oceans only if water can flow from any of its adjacent cells 
- *    to both Oceans. 
+ *  	- A cell is connected to both Pacific and Atlantic Oceans if it is connected (directly/indirectly)
+ *  	  to [a ("left" || "top") edge cell] && [a ("right" || "bottom") edge cell].
+ *    
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  - Can we check if a cell is connected to a [("left" || "top") edge cell] && 
+ *    [("right" || "bottom") edge cell] simultaneously?
+ *   
  *    
  *  - Brute Force Approach:
  *  
