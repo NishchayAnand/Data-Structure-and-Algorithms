2b92d72c82d66889ac1f70bf9838a4e3c0c8de5f
NishchayAnand <nishchay.anand63@gmail.com>
2024-03-18 11:18:13 +0530

    next smaller to the right

--- Stacks/src/LargestRectangleHistogram.java
+++ Stacks/src/LargestRectangleHistogram.java
@@ -1,18 +1,37 @@
 
-/* Problem Statement: Given an array of integers "heights" representing the histogram's bar height where 
- * 					  the width of each bar is 1, return the area of the largest rectangle in the 
+/* Problem Statement: Given an array of integers "heights" representing the height of histogram bars 
+ * 					  (width of each bar is 1), return the area of the largest rectangle in the 
  * 					  histogram.
  * 
  * General Observations:
  * 
- * 	- For any bar in histogram, the maximum area with the current bar's height = (index of NSE to the right
- *    - index of next smaller element to the left) - 1.
+ * 	- For a bar with height 'Hi', the maximum possible width of a rectangle with height = 'Hi' would be
+ * 	  (index of Nearest Smaller Height to the Right) - (index of Nearest Smaller Height to the Left) - 1.
  * 
  * */
 
 public class LargestRectangleHistogram {
 	
+	private static int getLargestRectangle(int[] heights) {
+		
+		int n = heights.length;
+		int maxArea = -1;
+		
+		int[] NSR = getNearestSmallerToRight(heights, n);
+		int[] NSL = getNearestSmallerToLeft(heights, n);
+		
+		for(int i=0; i<heights.length; i++) {
+			
+		}
+		
+		return maxArea;
+	}
+	
 	public static void main(String[] args) {
+		
+		int[] heights = {2,1,5,6,2,3};
+		
+		int result = getLargestRectangle(heights);
 		
 	}
 
--- Stacks/src/NextSmallerElementRight.java
+++ Stacks/src/NextSmallerElementRight.java
@@ -1,6 +1,78 @@
 import java.util.*;
 
+/* Problem Statement: 
+ * 
+ * 	- Given an input array of integers of length 'n', find the next smaller element for each of the array 
+ * 	  elements.
+ * 
+ * 	- NOTE: - Next Smaller Element for an array element is the first element to the right of that element 
+ * 	  	      which has a value strictly smaller than that element.
+ * 			- If for any array element the next smaller element does not exist, then next smaller element 
+ * 			  for that element is -1.
+ * 
+ * 	- Example: A = [2, 3, 1], Output = [1, 1, -1]. 
+ * 
+ * General Observations:
+ * 
+ * 	- Brute Force Approach: 
+ * 
+ * 		- Use a nested loop.
+ * 
+ * 		- Algorithm:
+ * 
+ * 			- Loop over each 'ith' element of the array:
+ *  
+ * 				- Loop over each 'jth' elements to the right of the current element: 
+ *  		  		- If 'jth' element < 'ith' element, 'jth' element is the next smaller element to 'ith'
+ *  				  element. 
+ * 
+ * 				- If there is no smaller 'jth' element, the next smaller element for that 'ith' element 
+ * 				  is considered -1.
+ * 
+ * 		- Time Complexity Analysis:
+ * 
+ * 			- In worst-case scenario, i.e, if the input array is sorted in ascending order, the algorithm
+ * 			  will perform n^2 operations. Hence, time complexity = O(n^2).
+ * 
+ * 		- Space Complexity Analysis:
+ * 
+ * 			- We will store the output in an array of size 'n'. Hence, space complexity = O(n).  
+ * 
+ * 		- The brute force approach is inefficient because we might revisit the same elements multiple 
+ * 		  times for different "i" values.
+ *    
+ * 
+ * 	- Optimized Approach:
+ * 
+ * 		- Process the input array from 'right' to 'left' and use a 'Stack' to store potential next 
+ * 		  smaller elements encountered during the iteration.
+ * 
+ * 		- Algorithm: 
+ * 
+ * 			- Loop over each 'ith' element of the input array from 'right' to 'left':
+ * 
+ * 				- Pop elements from the Stack until the 'ith' element is smaller than the element at 
+ * 				  the top of the stack;
+ * 
+ * 				- If there's still an element left in the stack, it is the next smaller element of the 
+ * 				  'ith' element.
+ * 
+ * 				- Push the 'ith' element onto the stack because as we move left in the array, 'ith' 
+ * 				  element might become the next smaller element for elements encountered later. 	
+ *
+ *		- Time Complexity Analysis:
+ * 
+ * 			- We will loop over the input array and perform few pop operations. Average time complexity
+ *   		  = O(n).
+ * 
+ * 		- Space Complexity Analysis:
+ * 
+ * 			- We will store the output in an array of size 'n'. Hence, space complexity = O(n). 		
+ * 
+ * */
+
 public class NextSmallerElementRight{
+	
     static ArrayList<Integer> nextSmallerElement(ArrayList<Integer> arr, int n){
         
         ArrayList<Integer> nse = new ArrayList<>();
