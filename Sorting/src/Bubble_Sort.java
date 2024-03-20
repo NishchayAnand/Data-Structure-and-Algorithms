d0a31e1627bc4b21246990b5334f1bd930654c16
NishchayAnand <nishchay.anand63@gmail.com>
2024-03-20 10:56:51 +0530

    minor additions in bubble sort

--- Sorting/src/Bubble_Sort.java
+++ Sorting/src/Bubble_Sort.java
@@ -4,24 +4,31 @@
  * 	- Ideology:
  * 
  * 		- Consider the array as 2  halves: 
- *    		(i) a group of sorted elements a.k.a "sorted sub-array"
- *          (ii) a group of unsorted elements a.k.a "unsorted sub-array".
+ *    		(i) a group of sorted elements a.k.a "sorted sub-array" (initially consider first element as part of sorted sub-array).
+ *          (ii) a group of unsorted elements a.k.a "unsorted sub-array" (initially consider all elements expect the first element as part of the un-sorted sub-array). 
  *  
  *  	- While the size of the unsorted array is not reduced to 0:
- *  		- Take the minimum element from the unsorted sub-array and place it at the end of the sorted 
- *   		  sub-array. 
+ *  		- Take the minimum element from the unsorted sub-array and place it at the start of the 
+ *            unsorted sub-array such that it becomes the last element of the sorted sub-array. 
  *     
  *  	- For an array A[0....n-1], looping from j=[n-1:1] and swapping the A[j] element with A[j-1] 
  *        element (i.e., element to its immediately left) if A[j] is smaller than A[j-1] will always end 
  *        up placing the minimum element at A[0], i.e., start of the array.
  *    
  *  - Algorithm:
- *  	- Loop from i = [0, n-2] --> for i=n-1, left sorted sub-array = A[0...n-2] and right unsorted sub-
- *                                              array = A[n-1] where A[n-1] is greater than all elements
- *                                              in left sub-array, therefore, at the correct location.
- *      	- Loop from j=[n-1, i+1]:
+ *  
+ *  	- Loop from i = [0, n-2] to fill the 'ith' position of the sorted sub-array:
+ *      	- Loop from j=[n-1, i+1] to find the minimum element in the unsorted sub-array and place it 
+ *      	  at the 'ith' index:
  *      		- if A[j] < A[j-1]:
  *      			- swap A[j], A[j-1] 
+ *      
+ *  	- NOTE:
+ *  
+ *  		- For i=(n-1), left sorted sub-array = A[0...n-2] and right unsorted sub-array = A[n-1], such
+ *  					   that A[n-1] is greater than all elements in left sorted sub-array, therefore, 
+ *  					   at the correct location.
+
  *      
  *	- Optimization: For any ith iteration, if no swaps happens, then we can say that the array is already 
  *					sorted and there is no need to process further.
