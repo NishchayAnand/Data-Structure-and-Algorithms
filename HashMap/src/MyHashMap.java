836fc93a9657b08d4d9e34bc9b2958d45ceef60b
NishchayAnand <nishchay.anand63@gmail.com>
2024-04-02 10:05:09 +0530

    added hashcode understanding

--- HashMap/src/MyHashMap.java
+++ HashMap/src/MyHashMap.java
@@ -53,30 +53,13 @@
  *  					 existing key-value pairs across the new buckets using the hash function. This 
  *  					 effectively reduces the average elements per bucket and minimizes collisions, 
  *  					 maintaining efficient performance.
- * 
- * 
- * 	- HashMap uses 4 private functions for its internal working. 
- * 
- * 		- initBucket(n): initiate an array of linkedLists of size 'n'.
- * 		- hashFunction(key): returns the bucket index of the linkedList containing the key-value pair.
- * 		- getIndexWithinBucket(key, bucket_index): iterates over the linkedList present at bucket_index
- * 												   and return the index where the key was present. If key 
- * 												   was not found, return -1.
- * 		- rehash(): re-calculates the hashcode of already stored entries (key-value pairs) to move them to
- * 				    another bigger size hashmap. 
- * 
- *  - Common HashMap functions:
- *  	
- *  	- get(key):
- *  	- put(key, value):
- *  	- containsKey(key):
- *  	- remove(key):
- *  	- size():
- *  	- keySet():
  *  
- *  - The hashCode() method is defined in the java.lang.Object class. It's a fundamental method that all 
- *    objects in Java inherit. The purpose of this method is to return an integer value that serves as a 
- *    hash code for the object.  	
+ *  - NOTE: 
+ *  
+ *  	- hashCode(): - A fundamental method that all objects in Java inherit. 	
+ *  				  - Defined in the java.lang.Object class. 
+ *  				  - Returns an integer that serves as a hash code for an object. 
+ *  				  - Different objects can have the same hash code (collision). 
  * 
  * */
 
