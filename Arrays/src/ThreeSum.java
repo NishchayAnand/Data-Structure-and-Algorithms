e14fd9b9ba6e4c0390f98c47c135571167a5df98
NishchayAnand <nishchay.anand63@gmail.com>
2024-04-24 11:00:08 +0530

    understanding 3 sum brute force approach

--- Arrays/src/ThreeSum.java
+++ Arrays/src/ThreeSum.java
@@ -6,23 +6,64 @@ import java.util.List;
 /* Problem Statement: Given an integer array 'nums', return all the triplets [nums[i], nums[j], nums[k]]
  * 					  such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
  * 
- * 					  NOTE: The solution set must not contain duplicate triplets.
+ * 					  NOTE: The output should not contain duplicate triplets.
  * 
  * General Observations:
  * 
- * 	- 
+ * 	- Brute Force Approach:
+ * 
+ * 		- Use "3 Nested Loops - {i,j,k}" to check every possible triplet. 
+ * 
+ * 		- Time Complexity: O(n^3).
+ * 
+ * 		- Space Complexity: O(1). 
+ * 
+ * 		- NOTE: Duplicate integers in the input array can lead to duplicate triplets (if order of integers
+ * 			    in the triplet does not matter). 
+ * 
+ * 				Example, for nums = [-1, 0, 1, 2, -1, 4, 1], the below mentioned triplets are all 
+ * 				duplicates. 
+ * 				
+ * 					1. [nums[0] = '-1', nums[1] = '0', nums[2] = '1'] 
+ * 					2. [nums[0] = '-1', nums[1] = '0', nums[6] = '1'] 
+ * 					3. [nums[1] = '0', nums[2] = '1', nums[4] = '-1']
+ * 		
+ * 		- Sorting the input array will get all the duplicate elements next to each other. 
+ * 
+ * 		- If the input array is sorted,
+ *  
+ * 			- we can skip finding the triplets whose first element = nums[i] if nums[i] == nums[i-1] 
+ *            (considering nums[i-1] exists) since those would have been explored in the previous ith iteration.
+ * 
+ * 			- we can skip finding the triplets whose middle element = nums[j] if nums[j] == nums[j-1] 
+ * 			  (considering nums[j-1] exists) since those would have been explored in the previous jth 
+ * 			  iterations. 
+ * 
+ * 			- we can skip finding the triplets whose last element = nums[k] if nums[k] = nums[k-1]
+ * 			  considering nums[k-1] exists) since those would have been explored in the previous kth
+ * 			  iterations. 
+ * 			
+ * 			  
  * 
  */
 
 public class ThreeSum {
 	
+	private static List<List<Integer>> threeSumBruteForce(int[] nums) {
+		
+		List<List<Integer>> output = new ArrayList<>();
+		
+		return output;
+		
+	}
+	
 	private static List<List<Integer>> threeSum(int[] nums) {
+		
+		List<List<Integer>> output = new ArrayList<>();
 		
 		Arrays.sort(nums);
 		
 		int n = nums.length;
-		
-		List<List<Integer>> output = new ArrayList<>();
 		
 		for(int i=0; i<n; i++) {
 			
@@ -72,7 +113,7 @@ public class ThreeSum {
 		
 		int[] nums = {-1,0,1,2,-1,-4};
 		
-		System.out.println("Triplets having sum = 0: " + threeSum(nums));	
+		System.out.println("Triplets having sum = 0 using brute force approach: " + threeSumBruteForce(nums));	
 
 	}
 
