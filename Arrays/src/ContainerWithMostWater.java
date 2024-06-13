f107213792d39023d454b272160e3b9113f94e95
NishchayAnand <nishchay.anand63@gmail.com>
2024-06-13 11:15:34 +0530

    understanding container with most water problem

---
+++ Arrays/src/ContainerWithMostWater.java
@@ -0,0 +1,36 @@
+
+/* Problem Statement: Given an array "height" containing n integers, each representing wall heights. Find 
+ * 					  two walls that together with the x-axis form a container, such that the container 
+ * 					  contains the most water.
+ * 
+ * General Observations:
+ * 
+ * 	- The amount of water held in a container made up of two walls = (Distance between two walls) x
+ * 	  (Distance between the x-axis and the height of the container, i.e., the minimum height of the two
+ *    walls).
+ * 
+ * 	- Brute Force Approach:
+ * 
+ * 		- Use a nested loop {i, j} to check every pair of walls and calculate the area of water they 
+ * 		  could hold.
+ * 
+ * 		- Time Complexity: O(n^2).
+ * 
+ * 		- Space Complexity: O(1).
+ * 
+ * 	- 
+ * 
+ * 	- Two Pointers Approach:
+ * 
+ * 		- 
+ * 
+ * */
+
+public class ContainerWithMostWater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
