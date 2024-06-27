c68f560e141dfa23c625f22509643687ec8c624c
NishchayAnand <nishchay.anand63@gmail.com>
2024-06-27 10:43:34 +0530

    done buy sell stock one transaction

--- Arrays/src/BuySellStocks.java
+++ Arrays/src/BuySellStocks.java
@@ -1,7 +1,8 @@
 
 /* Problem Statement: Given an array 'prices' where prices[i] is the price of a given stock on the ith day.
  * 					  You want to maximize your profit by choosing a single day to buy one stock and 
- * 				      choosing a different day in the future to sell that stock.
+ * 				      choosing a different day in the future to sell that stock (single transaction 
+ * 				      allowed).
  * 
  * 					  Return the maximum profit you can achieve from this transaction. 
  * 
@@ -9,7 +10,11 @@
  * 
  * General Observations:
  * 
- * 	- We need to find the pair of prices: {prices[i], prices[j]} whose difference is maximum, where i<j.
+ * 	- We need to find a pair of prices: {prices[i], prices[j]} such that prices[j]-prices[i] is maximum, 
+ *    where i<=j.
+ *    
+ *  - NOTE: prices[i] will always represent the buying price and prices[j] will always represent the 
+ *  		selling price. 
  * 
  *  - Brute Force Approach:
  *  
@@ -19,17 +24,63 @@
  *  
  *  	- Space Complexity: O(1).
  *  
- *  - 
+ *  - The maximum profit one can generate by selling at prices[j] would be by buying at the minimum
+ *    prices[i] in the range [0,..j]. 
  * 
- * 	- Sliding Window Technique:
+ * 	- Two Pointers Approach:
+ * 
+ * 		- We can maintain 2 pointers:
+ * 			- first to represent the index of the lowest price seeing so far.
+ * 			- second to represent the index of the current price.
+ * 	
+ * 		- The 2 pointers can be used to calculate the maximum profit one can generate by selling at any 
+ * 		  current price and buying at the lowest price observed so far.
+ * 
+ * 		- Algorithm:
+ * 
+ * 			- min_buy_idx = 0;  
+ * 			- curr_sell_idx = 0;
+ * 
+ * 			- maxProfit = 0;
+ * 
+ * 			- while curr_sell_idx < prices.length:
+ * 
+ * 				- if prices[curr_sell_idx] < prices[min_buy_idx]:
+ * 					- min_buy_idx = curr_sell_idx;
+ * 
+ * 				- currProfit = prices[curr_sell_idx] - prices[min_buy_idx];
+ * 				- maxProfit = max(maxProfit, currProfit);
+ * 
+ * 			- return maxProfit;
+ * 
+ * 		- Time Complexity: O(1).
+ * 	
+ * 		- Space Complexity: O(1).
  * 
  * */
 
 
 public class BuySellStocks {
+	
+	private static int maxProfit(int[] prices) {
+		
+		int maxProfit = Integer.MIN_VALUE;
+		int minPrice = Integer.MAX_VALUE;
+		
+		for(int price: prices) {
+			minPrice = Math.min(price, minPrice);
+			int profit = price - minPrice;
+			maxProfit = Math.max(maxProfit, profit);	
+		}
+		
+		return maxProfit;
+		
+	}
 
     public static void main(String[] args) throws Exception {
-        
+    	int[] prices = {7,1,5,3,6,4};
+    	int profit = maxProfit(prices);
+    	System.out.println("Maximum Profit: " + profit);
     }
 
 }
