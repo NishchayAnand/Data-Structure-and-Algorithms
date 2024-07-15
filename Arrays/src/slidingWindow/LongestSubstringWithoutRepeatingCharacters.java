a3da0dc968b07069a0dfce16e5dc26c19697a6fc
NishchayAnand <nishchay.anand63@gmail.com>
2024-07-15 10:41:24 +0530

    understanding sliding window technique

--- Arrays/src/slidingWindow/LongestSubstringWithoutRepeatingCharacters.java
+++ Arrays/src/slidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,9 @@
 package slidingWindow;
 
-/* Problem Statement: Given a string 's', find the length of the longest substring without repeating 
+import java.util.HashMap;
+import java.util.Map;
+
+/* Problem Statement: Given a string 'S', find the length of the longest substring without repeating 
  * 					  characters.
  * 
  * General Observations:
@@ -14,32 +17,70 @@ package slidingWindow;
  * 
  * 		- Time Complexity: O(n^2).
  * 		
- * 		- Space Complexity: O(1).
+ * 		- Space Complexity: O(n).
  * 
- *  - Let two pointers (i,j) represent the starting and ending indices of the longest possible substring 
- *    in the window [i,j]. This means that:
+ *  - If two pointers (i,j) represent the longest possible substring starting from index 'i', this means
+ *    the character at index 'j+1' already exists in the window [i,j]. 
  *    
- *    		(i) all characters in the range {0,j} have already been explored.
- *    	   (ii) the character at index 'j+1' already exists in the window [i,j]. 
- *    
- *  - Let (character at index 'j+1') = (character at index 'k'), where i<=k<=j. This means ...   
- *    
- *  - To find the longest 
- *    possible substring starting from index 'i+1', we only need explore the substrings starting from the 
- *    window [i+1, j>=k+1] (don't need to explore the substrings in the window [i+1, k]).
+ *  - If character at index 'j+1' = character at some index 'k', where i<=k<=j, we don't need to explore 
+ *    substrings in the window [i<=k, j+1] as they will contain duplicate characters. We can focus on 
+ *    exploring the window starting from [k+1, j+1] because any potential longer substring without 
+ *    duplicates must start after this point. 
  *    
  *  - Sliding Window Approach:
+ *  
+ *  	- Algorithm:
+ *  
+ *  		- Use a HashMap: 'hm' to store the last index of each character seen.
+ *  		- Let 'i' represent the beginning of the current window.
+ *  
+ *  		- Loop j = [0, n-1]: -> iterate through the string S
+ *  
+ *  			- if (hm contains S[j]) and (hm.get(S[j]) >= i): -> a duplicate character found in the current window
+ *  				- i = hm.get(S[j]) + 1;
+ *  
+ *  			- else:
+ *  				- maxLength = max(maxLength, j-i+1); -> compare the current window length (j - i + 1) 
+ *  				  									    with the previously found maximum length. 
+ *  
+ *  			- hm.put(S[j], j); -> stores the last index of S[j].
+ *  			
+ *  		- return maxLength;
+ *  
+ *  	- Time Complexity: O(n).
+ *  
+ *  	- Space Complexity: O(n).
  * 
  * */
 
 public class LongestSubstringWithoutRepeatingCharacters {
 	
 	private static int lengthOfLongestSubstring(String s) {
-		return s.length();
+		
+		Map<Character, Integer> hm = new HashMap<>();
+		int maxLength = 0;
+		int i=0;
+		
+		for(int j=0; j<s.length(); j++) {
+			
+			char ch = s.charAt(j);
+			if( hm.containsKey(ch) && hm.get(ch) >= i) {
+				i = hm.get(ch) + 1;
+			} else  {
+				maxLength = Math.max(maxLength, j-i+1);
+			}
+			
+			hm.put(s.charAt(j), j);
+		}
+		
+		return maxLength;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String s = "tmmzuxt"; //abcbcabb
+		int len = lengthOfLongestSubstring(s);
+		System.out.println("Length of longest substring in " + s + " without duplicate character: " + len);
 
 	}
 
