508f5d9e9a321e57863bada023005d0ee58b26b5
NishchayAnand <nishchay.anand63@gmail.com>
2024-07-24 11:03:44 +0530

    understanding cycle detection in Linked List

--- Linked List/src/DetectLoopInLinkedList.java
+++ Linked List/src/DetectLoopInLinkedList.java
@@ -3,55 +3,57 @@
  * 
  * General Observations:
  * 
+ * 	- Each node object in a linked list is unique, even if the values they hold are the same.
+ * 
  * 	- Brute Force Approach: 
  * 
- * 		- use a hashMap(keys = Node, values = boolean) to mark the nodes as visited as we iterate over the
- * 		  linked list.
+ * 		- Iterate over the Linked List and store each visited Node in a HashSet. While iterating, if we
+ * 		  jump upon a node which is already present in the HashSet, then we have a Cycle in Linked List.
  * 
- * 		NOTE: make sure to use key as Nodes to make sure the algorithm works fine for duplicate node 
- * 			  values.
+ * 		- NOTE: Store the Node objects in the HashSet to make sure the algorithm works fine for duplicate 
+ * 				node values.
  * 
- *  	- Time Complexity Analysis: O(N).
+ *  	- Time Complexity Analysis: O(n).
  *  
- *  	- Space Complexity Analysis: O(N).
+ *  	- Space Complexity Analysis: O(n).
  *  
- *  - Floyd cycle detection:
- *  
- *  	- If you have two pointers on an infinite strip separated by a distance of k units, and you move 
- *  	  the first pointer by 2 units and the second pointer by 1 unit in each iteration, the distance 
- *  	  between the two pointers will decrease by 1 in each iteration.
+ *  - Floyd Cycle Detection Approach: 
  *  	
- *  	- Given that the distance between the pointers decreases by 1 unit with every iteration, 
- *  	  eventually, the pointers will collide when the distance between them becomes 0.
+ *  	- Assume you have two pointers on an infinite strip separated by a distance of 'k' units. 
  *  
- *  	- A cycle is like a infinite strip. 
- * 	
- * 
+ *  	- If you starting moving the first pointer (behind) by 2 units and the second pointer (ahead) by 1
+ *  	  unit, the distance between the two pointers will start decreasing by 1 unit in each iteration 
+ *  	  and will eventually reduce to 0, i.e., the two pointers will collide.
+ *  	
+ *  	- A cycle in a Linked List can be thought of as an infinite strip. 
+ *  
+ *  	- Time Complexity: ?
+ *  
+ *  	- Space Complexity: O(1).
+ *  
  * */
 
 
 public class DetectLoopInLinkedList {
 	
-	// return node represent the node where the fast and slow pointers collided.
-	public static Node detectCycle(Node head) {
+	public static Boolean hasCycle(Node head) {
 
         Node slow = head;
         Node fast = head;
 
-        while(fast!=null && fast.next!=null){
-            fast = fast.next.next;
-            slow = slow.next;
+        while(fast!=null && fast.next!=null){ // could be figured out through dry run.
+            fast = fast.next.next; // jump of 2 units.
+            slow = slow.next;	   // jump of 1 unit.
             if(slow == fast){
-                return slow;
+                return true;
             }
         }
 
-        return null;
+        return false;
         
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
