a717e4737a37b05118f964cddb02375f177c0ecc
NishchayAnand <nishchay.anand63@gmail.com>
2024-07-26 11:01:01 +0530

    Understanding LRU Cache

--- Linked List/src/LRUCache.java
+++ Linked List/src/LRUCache.java
@@ -4,11 +4,31 @@
  * 
  * General Observations:
  * 
- * 	- LRU Cache prioritizes recently used data and evicts least recently used data.
+ * 	- A Cache is a temporarily storage used to store frequently accessed data.
  * 
- * 	- The idea is that data accessed recently is more likely to be accessed again in the near future. When
- *    the cache is full and a new item needs to be added, the least recently used item is removed to make 
- *    space.
+ * 	- Cache usually store data as key-value pairs.
+ * 
+ * 	- LRU (Least Recently Used) Cache is is a type of Cache that is optimized to work when with limited 
+ * 	  cache space.
+ * 
+ * 	- Ideology:
+ * 
+ * 		- Prioritizes Recently Used Data: The idea is that data accessed recently is more likely to be 
+ * 										  accessed again in the near future.
+ * 
+ * 		- Evicts Least Recently Used Data: When the cache is full and a new item needs to be added, the 
+ * 										   least recently used item is removed to make space.
+ * 
+ * 	- Requirements:
+ * 
+ * 		- A Data Structure with a fixed Capacity that allows O(1) storage and retrieval: We can use 
+ * 		  HashMap. 
+ * 
+ * 		- A Data Structure to keep track of the Order of Access: We can use a Linked List where:
+ * 			- 'Head' should always point to the 'Most_Recently_Accessed_Object'
+ * 			- 'Tail' should always point to the 'Least_Recently_Accessed_Object'
+ * 
+ *   - Implementation: 
  * 
  * */
 
@@ -16,7 +36,6 @@ public class LRUCache {
 	
 	public LRUCache(int capacity) {
 		// Initialize the LRU cache with positive size capacity.
-		
 	}
 	
 	public int get(int key) {
@@ -28,7 +47,6 @@ public class LRUCache {
     	// Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. 
     	// If the number of keys exceeds the capacity from this operation, evict the least recently used 
     	// key. The functions get and put must each run in O(1) average time complexity.
-        
     }
 
 	public static void main(String[] args) {
