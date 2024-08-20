c362bf94780200729339822bd49499a8eb2f7820
NishchayAnand <nishchay.anand63@gmail.com>
2024-08-21 01:29:10 +0530

    understanding longest consecutive sequence

--- HashMap/src/LongestConsecutiveSequence.java
+++ HashMap/src/LongestConsecutiveSequence.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 /* Problem Statement: Given an unsorted array of integers 'nums', return the length of the longest 
  * 					  consecutive elements sequence.
@@ -6,67 +8,12 @@ import java.util.HashSet;
  * 					  For example, for nums = [100,4,200,1,3,2], the LCS = [1,2,3,4]. Hence, the length
  * 					  of the longest consecutive elements subsequence is 4.
  * 
- * General Observaions:
+ * General Observaions: 
  * 
- * 	- If all elements in the 'nums' array is unique, the length of the longest consecutive elements
- * 	  sequence = 1. 
+ * 	- Each element can either be the "starting element of a subsequence" or "part of a subsequence".
  * 
- * 	- Brute Force Approach:
- * 
- * 		- Algorithm:
- * 
- * 			- Sort the 'nums' array.
- * 				- sorted_nums = [1,2,3,4,100,200];
- * 
- * 			- Initialize two variables: 
- * 				- First, to store the length of the current subsequence.
- * 						- current_length = 1;
- * 			  	- Second, to store the length of the longest subsequence found so far.
- * 						- longest_length = 1;
- * 
- * 			- Loop from i = [1, n) to iterate over the sorted 'nums' array:
- * 				- If nums[i] == nums[i-1] + 1:
- * 					- Increment current_length by 1;
- * 				- Else:
- * 					- Update longest_length = max(longest_length, current_length);
- * 					- Reset current_length = 1;
- * 
- * 			- return longest_length;
- *
- * 		- Time Complexity Analysis:
- * 			
- * 			- The number of operations performed in:
- * 				- sorting the 'nums' array will be of the order (n*logn).
- * 				- looping over the sorted 'nums' array will be of order (n).
- * 			
- * 			- Hence, overall time complexity = O(n*logn).
- * 
- * 		- Space Complexity = O(1). 
- * 
- * 	- Optimized Approach:
- * 
- * 		- Each element can either be the "starting element of a subsequence" or "part of a subsequence".
- * 
- * 		- If we know the starting element of each consecutive subsequence, we can easily figure out 
+ * 	- If we know the starting element of each consecutive subsequence, we can easily figure out 
  * 		  the longest consecutive subsequence. 
- * 
- *  	- Algorithm:
- *  		
- *  		- Create a HashSet to store all the elements of the 'nums' array. (#1)
- *  
- *  		- Initialize longest_length = 0.
- *  
- *  		- Loop from i = [1, n) to iterate over the 'nums' array: (#2)
- *  			- If HashSet contains (nums[i]-1):
- *  				- Do nothing, since, nums[i] is part of a subsequence which would surely be explored in the else section.
- *  			- Else:
- *  				- current_element = nums[i];
- *  				- while HashSet contains (current_element+1): // explore the consecutive sequence starting from nums[i].
- *  					- Increment current_length by 1;
- *  					- Increment curr_element by 1;
- *  				- Update longest_length = max(longest_length, current_length);
- *  
- *  		- return longest_length;
  *  
  *  	- Time Complexity Analysis:
  *  		
@@ -92,36 +39,61 @@ import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
 	
+	public static int longestConsecutiveBF(int[] nums) {
+
+        if(nums.length == 0) return 0;
+		
+		int maxLength = 1;
+			
+		Set<Integer> hs = new HashSet<>();
+		for(int num: nums) {
+			hs.add(num);
+		}
+			
+		for(int startNum: hs) {
+			int currLength = 1;
+            int nextNum = startNum+1;
+			while(hs.contains(nextNum)) {
+				currLength++;
+				nextNum++;
+			}
+			maxLength = Math.max(maxLength, currLength);
+		}
+			
+		return maxLength;
+        
+    }
+
 	private static int longestConsecutive(int[] nums) {
 		
-		HashSet<Integer> hs = new HashSet<>();
+		if(nums.length == 0) return 0;
 		
-		for(int el: nums) {
-			hs.add(el);
+		int maxLength = 1;
+			
+		Set<Integer> hs = new HashSet<>();
+		for(int num: nums) {
+			hs.add(num);
 		}
-		
-		int longest_length = 0;
-		
-		for(int el: nums) {
-			// el marks the start of a subsequence.
-			if(!hs.contains(el-1)) {
-				int current_length = 1;
-				int current_element = el;
-				while(hs.contains(current_element+1)) {
-					current_length++;
-					current_element++;
-				}
-				longest_length = Math.max(longest_length, current_length);
-			}
+			
+		for(int startNum: hs) {
+            if(!hs.contains(startNum-1)) {
+                int currLength = 1;
+                int nextNum = startNum+1;
+			    while(hs.contains(nextNum)) {
+				    currLength++;
+				    nextNum++;
+			    }
+			    maxLength = Math.max(maxLength, currLength);
+            }	
 		}
-		
-		return longest_length;
+			
+		return maxLength;
 		
 	}
 
 	public static void main(String[] args) {
 		
-		int[] nums = {100, 4, 200, 1, 3, 2};
+		int[] nums = {1,2,0,1};
 		
 		System.out.println("Length of the longest consecutive elements subsequence: " + longestConsecutive(nums));
 
