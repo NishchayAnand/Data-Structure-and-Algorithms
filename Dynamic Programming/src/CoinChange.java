9ea8492485d1dfdb0550ba6ab92c0f0e654fcee4
nishchayanand <nishchay.anand63@gmail.com>
2024-10-02 07:35:46 +0530

    tabulation - coin change problem

--- Dynamic Programming/src/CoinChange.java
+++ Dynamic Programming/src/CoinChange.java
@@ -29,7 +29,7 @@
 	    		- exclude = F(coins, n-1, amount);
 
 	    		- if coins[n-1] <= amount:
-	    			- include = F(coins, n, amount-coins[n-1]);
+	    			- include = 1 + F(coins, n, amount-coins[n-1]);
 
 				- return min(exclude, include);
 
@@ -54,7 +54,19 @@
 
 			- Each sub-problem can be represented using 2 varying parameters: 'n' and 'amount'.
 
-			- We can use a 2D array (matrix): mem[n+1][amount+1] to cache the result of each sub-problem.
+			- We can use a 2D array (matrix): mem[n+1][amount+1] to cache the result of each sub-problem,
+			  where each cell represents a unique sub-problem.
+
+			- Time Complexity: O(n*amount).
+
+			- Space Complexity: O(n*amount).
+
+		- The 2D memoization array is getting fill from bottom to top, i.e., from smallest
+		  sub-problem to the largest sub-problem.
+
+		- Optimization - 2D Tabulation Approach:
+
+			- We can use a nested loop and the recursive steps to fill the 2D memoization array.
 
 			- Every mem[0][amount] = Integer.MAX_VALUE;
 
@@ -62,14 +74,22 @@
 
 			- mem[0][0] = 0;
 
-			- Time Complexity: O(n^2).
+			- Time Complexity: O(n*amount).
 
-			- Space Complexity: O(n^2).
+			- Space Complexity: O(n^amount) -> We won't have the overhead associated with
+											   recursive call stack.
 
+		- For any mem[r][c], we only need to know mem[r-1][c] and mem[r][c-coins[r-1]], mem[r-1][c]
+		  and mem[r][c-coins[r-1]] has already been computed in the previous iteration.
 
+		- Optimization - 1D Tabulation Approach:
 
+			- Use two 1D arrays to keep track of the mem[r-1][c] and mem[r][c-coins[r-1]]
+			  sub-problem.
 
+			- Time Complexity: O(n*amount).
 
+			- Space Complexity: O(amount).
 
 */
 
@@ -125,6 +145,42 @@ public class CoinChange {
 
 	}
 
+	private static int coinChangeTabulation(int[] coins, int n, int amount, int[][] mem) {
+		for(int r=1; r<=n; r++) {
+			for(int c=1; c<=amount; c++) {
+				int exclude = mem[r-1][c];
+				int include = Integer.MAX_VALUE;
+				if(coins[r-1]<=c){
+					include = mem[r][c-coins[r-1]];
+					if(include != Integer.MAX_VALUE) {
+						include+=1;
+					}
+				}
+				mem[r][c] = Math.min(exclude, include);
+			}
+		}
+		return mem[n][amount];
+	}
+
+	private static int coinChange1DTabulation(int[] coins, int n, int amount, int[] mem) {
+		int[] curr = new int[amount+1];
+		for(int r=1; r<=n; r++) {
+			for(int c=1; c<=amount; c++) {
+				int exclude = mem[c];
+				int include = Integer.MAX_VALUE;
+				if(coins[r-1]<=c){
+					include = curr[c-coins[r-1]];
+					if(include != Integer.MAX_VALUE) {
+						include+=1;
+					}
+				}
+				curr[c] = Math.min(exclude, include);
+			}
+			mem = curr.clone();
+		}
+		return mem[amount];
+	}
+
 	public static int coinChange(int[] coins, int amount) {
 		int n = coins.length;
 
@@ -132,13 +188,30 @@ public class CoinChange {
 		//int result = coinChangeRecursive(coins, n, amount);
 
 		// Optimized - Memoization
-		int[][] mem = new int[n+1][amount+1];
+		int[][] mem2D = new int[n+1][amount+1];
 		for(int r=0; r<=n; r++){
 			for(int c=0; c<=amount; c++) {
-				mem[r][c] = -1;
+				mem2D[r][c] = -1;
 			}
 		}
-		int result = coinChangeMemoized(coins, n, amount, mem);
+		//int result = coinChangeMemoized(coins, n, amount, mem2D);
+
+		// Optimized - 2D Tabulation
+		for(int c=0; c<=amount; c++) {
+			mem2D[0][c] = Integer.MAX_VALUE;
+		}
+		for(int r=0; r<=n; r++) {
+			mem2D[r][0] = 0;
+		}
+		//int result = coinChangeTabulation(coins, n, amount, mem2D);
+
+		// Optimized - 1D Tabulation
+		int[] mem1D = new int[amount+1];
+		//curr[0] = 0; will be set by default.
+		for(int c=1; c<=amount; c++) {
+			mem1D[c] = Integer.MAX_VALUE;
+		}
+		int result = coinChange1DTabulation(coins, n, amount, mem1D);
 
 		return result == Integer.MAX_VALUE ? -1 : result;
 	}
