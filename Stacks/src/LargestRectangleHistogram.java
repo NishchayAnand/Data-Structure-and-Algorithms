885c736f90d0cce62c7854bdb2e407a09109f231
nishchayanand <nishchay.anand63@gmail.com>
2024-11-24 14:48:19 +0530

    understanding largest rectangle histogram - stack

--- Stacks/src/LargestRectangleHistogram.java
+++ Stacks/src/LargestRectangleHistogram.java
@@ -1,104 +1,109 @@
-import java.util.ArrayDeque;
 
-/* Problem Statement: Given an array "heights" holding 'n integers', each representing the height of a
- * 					  histogram bar (width of each bar is 1), return the area of the largest possible 
- * 					  rectangle in the histogram.
- * 
- * General Observations:
- * 
- * 	- For a bar with height 'Hi', the maximum possible width of a rectangle with height = 'Hi' would be
- * 	  (index of Nearest Smaller Height to the Right) - (index of Nearest Smaller Height to the Left) - 1.
- * 
- * 	- Consider the nearest smaller element to the:
- * 			- left of the first bar as -1.
- * 			- right of the last bar as 'n', length of input array "heights".
- * 
- * 	- Time Complexity = O(n).
- * 
- * 	- Space Complexity = O(n).
- * 
- * */
+import java.util.Stack;
+
+/*
+
+	Problem Statement: Given an array of integers 'heights' representing the histogram's bar height
+	 				   where the width of each bar is 1, return the area of the largest rectangle in
+	 				   the histogram.
+
+
+	General Observations:
+
+		- A valid rectangle can be represented as a pair of bars {i,j} where:
+			  				(i) i<=j,
+			  			   (ii) height[i] <= height of all bars in the range [i+1, j].
+
+		- Brute Force Approach:
+
+			- Use a nested loop {i,j} to iterate over all valid rectangles.
+
+			- For every 'ith' bar, start the inner loop from 'j=i' and expand until in the right
+			  until you reach a bar where height[j] < height[i].
+
+			- Area of a valid rectangle {i,j} = (j-i+1)*height[i].
+
+			- Time Complexity: O(n^2).
+
+			- Space Complexity: O(1).
+
+		- For every 'ith' bar, width of the largest rectangle with height = height[i] =
+		  [NearestSmallerRightIndex - NearestSmallerLeftIndex - 1].
+
+		- Pre-computed Monotonic Stacks Approach:
+
+			- Algorithm:
+				- NSL = nearestSmallerLeftIndex(heights);
+				- NSR = nearestSmallerRightIndex(heights);
+				- Loop from i = [0, n):
+					- currArea = heights[i]*(NSR[i]-NSL[i]-1);
+					- maxArea = max(maxArea, currArea);
+				- return maxArea;
+
+			- Time Complexity: O(n).
+
+			- Space Complexity: O(n).
+
+		- Single Stack Approach:
+
+
+
+*/
 
 public class LargestRectangleHistogram {
 	
-	private static int[] getNearestSmallerRightIndex(int[] heights, int n) {
-		
-		int[] output = new int[n];
-		
-		ArrayDeque<Integer> stack = new ArrayDeque<>();
-		
-		for(int i=n-1; i>=0; i--) {
-			
-			while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]) {
-				stack.pop();
-			}
-			
-			if(stack.isEmpty()) {
-				output[i] = n;
-			} else {
-				output[i] = stack.peek();
-			}
-			
-			stack.push(i);
-		}
-		
-		return output;	
-		
-	}
-	
-	private static int[] getNearestSmallerLeftIndex(int[] heights, int n) {
-		
-		int[] output = new int[n];
-		
-		ArrayDeque<Integer> stack = new ArrayDeque<>();
-		
-		for(int i=0; i<n; i++) {
-			
-			while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]) {
-				stack.pop();
-			}
-			
-			if(stack.isEmpty()) {
-				output[i] = -1;
-			} else {
-				output[i] = stack.peek();
-			}
-			
-			stack.push(i);
-		}
-		
-		return output;	
-		
-	}
-	
-	private static int getLargestRectangle(int[] heights) {
-		
+	private static int[] nearestSmallerRightIndex(int[] heights) {
+
 		int n = heights.length;
-		int maxArea = -1;
+		int[] NSR = new int[n];
 		
-		int[] NSR = getNearestSmallerRightIndex(heights, n); // O(n)
-		int[] NSL = getNearestSmallerLeftIndex(heights, n);	 // O(n)
-		
-		for(int i=0; i<n; i++) {							 // O(n)
-			
-			int width = (NSR[i] - NSL[i]) - 1;
-			
-			int area = width*heights[i];
-			maxArea = Math.max(maxArea, area);
-			
+		Stack<Integer> stack = new Stack<>(); // monotonically decreasing
+		for(int i=n-1; i>=0; i--) {
+			while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+			if(stack.isEmpty()) NSR[i] = n;
+			else NSR[i] = stack.peek();
+			stack.push(i);
 		}
 		
+		return NSR;
+		
+	}
+	
+	private static int[] nearestSmallerLeftIndex(int[] heights) {
+
+		int n = heights.length;
+		int[] NSL = new int[n];
+		
+		Stack<Integer> stack = new Stack<>();
+		for(int i=0; i<n; i++) {
+			while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+			if(stack.isEmpty()) NSL[i] = -1;
+			else NSL[i] = stack.peek();
+			stack.push(i);
+		}
+		
+		return NSL;
+		
+	}
+	
+	public static int getLargestRectangle(int[] heights) {
+
+		int[] NSR = nearestSmallerRightIndex(heights);
+		int[] NSL = nearestSmallerLeftIndex(heights);
+
+		int maxArea = 0;
+		for(int i=0; i<heights.length; i++) {
+			int currArea = heights[i]*(NSR[i] - NSL[i] - 1);
+			maxArea = Math.max(maxArea, currArea);
+		}
+
 		return maxArea;
 	}
 	
 	public static void main(String[] args) {
-		
 		int[] heights = {2,1,5,6,2,3};
-		
 		int result = getLargestRectangle(heights);
-		
 		System.out.println("Area of largest possible rectangle: "+result);
-		
 	}
 
 }
