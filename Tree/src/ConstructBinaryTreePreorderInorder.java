ea3a8953699e17b0ed7d2012033b192c881b9550
nishchayanand <nishchay.anand63@gmail.com>
2024-12-23 11:42:33 +0530

    construct binary tree using preorder and inorder traversal

---
+++ Tree/src/ConstructBinaryTreePreorderInorder.java
@@ -0,0 +1,19 @@
+
+/*
+
+    Problem Statement: Given two integer arrays 'preorder' and 'inorder' where 'preorder' is the preorder traversal of
+                       a binary tree and 'inorder' is the inorder traversal of the same tree, construct and return the
+                       binary tree.
+
+    General Observations:
+
+        - The first element in the 'preorder' list represents the 'root' of the tree.
+
+        - All the elements to the left of the 'root' in the 'inorder' list belongs to the left subtree of the root.
+          Similarly, all the elements to the right of the 'root' in the 'inorder' list belongs to the right subtree of
+          the root.
+
+*/
+
+public class ConstructBinaryTreePreorderInorder {
+}
