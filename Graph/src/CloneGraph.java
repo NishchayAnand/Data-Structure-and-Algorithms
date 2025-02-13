f210144a8c34663a0362daca919cf5b9c1de931e
nishchayanand <nishchay.anand63@gmail.com>
2025-02-13 10:18:02 +0530

    clone graph

--- Graph/src/CloneGraph.java
+++ Graph/src/CloneGraph.java
@@ -1,128 +1,119 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
+/*
 
-/* Problem Statement: Given a reference of a node in a connected undirected graph, return a deep copy 
- * 					  a.k.a "clone of the graph".
- * 
- * General Observations:
- * 			
- * - We can use Depth-First-Search to traverse over the given graph.
- * 
- * - Hypothesis:
- * 
- * 		- DFS(node, cloned) will create a clone of 'node' and all its neighbours (direct/indirect) and 
- * 		  return the cloned 'node'.
- * 
- * - Recursive Steps:
- * 
- * 		- clone = Node(node.val);
- * 		- cloned(node);
- * 		- for each neighbour of node:
- * 			- neighbourClone = DFS(neighbour, cloned);
- * 			- clone.neighbors.add(neighbourClone);
- * 		- return clone;
- * 
- * - Base Condition:
- * 		
- * 		- if cloned.contains(node):
- * 			- return node->clone;
- * 
- * - NOTE: We need to maintain a "mapping of each node and its clone". Hence, 'cloned' should be a 
- * 		   HashMap where:
- * 				- original node -> key,
- * 				- clone node -> value.
- * 
- * - Time Complexity Analysis:
- * 
- * 		- "clone = Node(node.val)" will be executed 'n' (no. of vertices) times.
- *  
- *  	- "cloned.neighbors.add(nbrClone)" will be executed 'm' (no of edges) times.
- *  
- *  	- Therefore, time complexity = O(n+m).
- * 
- * - Space Complexity Analysis:
- * 		
- * 		- We are using a 'isCloned' HashMap which will store mapping of each cloned vertex. Hence, 
- *        the space consumed by the HasMap would be of order "n". 
- *        
- *      - Therefore, space complexity = O(n).
- * 
- * */
+	Problem Statement:
+
+		- Given a reference of a node in a connected undirected graph. return a deep copy (clone) of the graph.
+
+		- NOTE:
+			- Each node's value is the same as the node's index (1-indexed).
+			- The given node will always be the first node with val = 1.
+
+	General Observations:
+
+		- The problem requires us to traverse the entire graph and create copies of each node while maintaining their
+		  connections.
+
+		- Intuition:
+			- Use Depth-First-Search to traverse over the given graph.
+
+		- Algorithm:
+
+			- Hypothesis:
+				- F(node) will return a deep copy of node and all its neighbors.
+
+			- Recursive Steps:
+				- clonedNode = Node(node);
+				- for each neighbor in node:
+					- clonedNeighbor = F(neighbor);
+					- clonedNode.add(clonedNeighbor);
+				- return clonedNode;
+
+			- Base Conditions:
+				- if clonedNode of node exists: return clonedNode;
+
+			NOTE: Use a HashMap to keep track of cloned nodes.
+
+			- Time Complexity: O(V+E)
+
+			- Space Complexity: O(V)
+
+*/
 
 public class CloneGraph {
-	
-	private static Node createSampleInput() {
-		
-		Node node1 = new Node(1);
-		Node node2 = new Node(2);
-		Node node3 = new Node(3);
-		Node node4 = new Node(4);
-		
-		node1.neighbors.add(node2);
-		node1.neighbors.add(node4);
-		
-		node2.neighbors.add(node1);
-		node2.neighbors.add(node3);
-		
-		node3.neighbors.add(node2);
-		node3.neighbors.add(node4);
-		
-		node4.neighbors.add(node1);
-		node4.neighbors.add(node3);
-		
-		return node1;
+
+	static class Node {
+
+		public int val;
+		public List<Node> neighbors;
+
+		public Node() {
+			val = 0;
+			neighbors = new ArrayList<Node>();
+		}
+
+		public Node(int _val) {
+			val = _val;
+			neighbors = new ArrayList<Node>();
+		}
+
+		public Node(int _val, ArrayList<Node> _neighbors) {
+			val = _val;
+			neighbors = _neighbors;
+		}
+
 	}
 	
-	private static Node clone(Node node, HashMap<Node, Node> isCloned) {
+	private static Node DFS(Node node, Map<Node, Node> clones) {
+
+		// Base Conditions:
+		if(clones.containsKey(node)) return clones.get(node);
+
+		// Recursive Steps:
+		Node clonedNode = new Node(node.val);
+		clones.put(node, clonedNode);
 		
-		if(isCloned.containsKey(node)) {
-			return isCloned.get(node);
+		for(Node neighbor: node.neighbors) {
+			Node clonedNeighbor = DFS(neighbor, clones);
+			clonedNode.neighbors.add(clonedNeighbor);
 		}
 		
-		Node cloned = new Node(node.val);
-		isCloned.put(node, cloned);
-		
-		for(Node nbr: node.neighbors) {
-			Node nbrClone = clone(nbr, isCloned);
-			cloned.neighbors.add(nbrClone);
-			
-		}
-		
-		return cloned;
+		return clonedNode;
 		
 	}
 	
     private static Node cloneGraph(Node node) {
-    	
-    	HashMap<Node, Node> isCloned = new HashMap<>();
-    	
-        return clone(node, isCloned);
-        
+		if(node == null) return null;
+    	Map<Node, Node> clones = new HashMap<>();
+        return DFS(node, clones);
     }
 
 	public static void main(String[] args) {
-		
-		Node node = createSampleInput();
-	
-		Node clonedNode = cloneGraph(node);
+
+		int[][] adj = {{2,4},{1,3},{2,4},{1,3}};
+		int V = adj.length;
+
+		// Create vertices
+		Node[] nodes = new Node[V];
+		for(int i=0; i<V; i++) {
+			nodes[i] = new Node(i+1);
+		}
+
+		// Create edges
+		for(int i=0; i<V; i++) {
+			Node node = nodes[i];
+			int[] edges = adj[i];
+			for(int edge: edges) {
+				Node neighbor = nodes[edge-1];
+				node.neighbors.add(neighbor);
+			}
+		}
+
+		Node clonedRootNode = cloneGraph(nodes[0]);
 		
 	}
 
