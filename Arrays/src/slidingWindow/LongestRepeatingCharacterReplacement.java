7b01bd6b6d919339826cff48aa212103404224e3
nishchayanand <nishchay.anand63@gmail.com>
2025-03-01 14:10:35 +0530

    longest repeating character replacement - array - greedy - sliding window

--- Arrays/src/slidingWindow/LongestRepeatingCharacterReplacement.java
+++ Arrays/src/slidingWindow/LongestRepeatingCharacterReplacement.java
@@ -10,9 +10,8 @@ package slidingWindow;
 
     General Observations:
 
-        - Intuition:
-            - Explore all substrings where you can replace at most k characters to make all the characters the same and
-              keep track of the longest one.
+        - Basic Intuition: Explore all substrings where you can replace at most k characters to make all the characters
+                           the same and keep track of the longest one.
 
         - Brute Force Approach:
 
@@ -21,7 +20,7 @@ package slidingWindow;
 
             - Algorithm:
                 - maxLength = 0;
-                - Loop from i = [0, n-1] to explore all possible starting indices:
+                - Loop from i = [0, n-1] to explore all starting indices:
                     - Loop from j = [i, n-1] to explore all possible ending indices for substrings starting at s[i]:
                         - if (length of substring s[i:j] - count of most frequently occurring character) <= k:
                             - maxLength = max(maxLength, length of substring s[i:j]);
@@ -29,11 +28,42 @@ package slidingWindow;
 
             - Time Complexity: O(n^2).
 
-            - Space Complexity Analysis:
-                - We may need to use a hashMap or an array of size 26 to store the frequency of all the substrings
-                  starting with each character s[i].
-                - Thus, total space complexity will be O(26) ~ O(1).
+            - Space Complexity Analysis: We need to use a HashMap or an array of size 26 to efficiently track character
+                                         frequencies for each substring window. Thus, total space complexity will be
+                                         O(26) ~ O(1).
 
+        - Redundant Operations: If substring s[i,j] represents the longest possible substring starting from s[i], then
+                                any substring starting from a later index s[i+k, j] where i < k ≤ j will be shorter and
+                                is therefore redundant to explore separately.
+
+        - Sliding Window Approach:
+
+            - Use two pointers (left and right) to define a substring window that expands right while valid (replacements ≤ k)
+              and shrinks left only when replacements exceed k.
+
+            - NOTE: Removing just one character from the left is enough to make the window valid again.
+
+            - Algorithm:
+                - Initialize the current substring window (left = 0, right = 0);
+                - While the current substring window remains in bound (right < n):
+                    - Increase the frequency of character at index = 'right' by 1;
+                    - Update the most frequent character in the current window;
+                    - Check if s[left:right] is a valid substring (replacements ≤ k):
+                        - Update the length of the longest substring where you can replace at most k characters to make
+                          all the characters the same;
+                        - Expand the current window towards right by 1;
+                    - Else:
+                        - Decrease the frequency of character at index = 'left by 1;
+                        - Shrink the current window from left by 1;
+                        - Expand the current window towards right by 1; // IMPORTANT - otherwise character at index = 'right'
+                                                                                       would be processed twice.
+                - Return the length of the longest substring;
+
+            - Time Complexity Analysis: In the worst-case scenario (string containing unique characters and k = 1), each
+                                        character would be visited twice (first time by the right pointer and later by the
+                                        left pointer). Thus, total time complexity will be O(2n) ~ O(n).
+
+            - Space Complexity: O(26) ~ O(1).
 
 */
 
@@ -54,9 +84,27 @@ public class LongestRepeatingCharacterReplacement {
         return maxLength;
     }
 
+    public static int characterReplacement(String s, int k) {
+        int n = s.length();
+        int maxLength = 0, maxFreq = 0, left = 0;
+        int[] freqMap = new int[26];
+
+        for(int right=0; right<n; right++) {
+            maxFreq = Math.max(maxFreq, ++freqMap[s.charAt(right) - 'A']);
+            int currWindowLength = right - left + 1;
+            if ( currWindowLength - maxFreq > k ) {
+                freqMap[s.charAt(left) - 'A']--;
+                left++;
+            } else {
+                maxLength = Math.max(maxLength, currWindowLength);
+            }
+        }
+        return maxLength;
+    }
+
     public static void main(String[] args) {
         String s = "AABABBA";
         int k = 2;
-        System.out.println(characterReplacementBruteForce(s,k));
+        System.out.println(characterReplacement(s,k));
     }
 }
