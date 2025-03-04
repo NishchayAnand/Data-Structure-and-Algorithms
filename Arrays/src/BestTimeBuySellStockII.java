19bbbbd9b70195372f08473427f07188b96f500f
nishchayanand <nishchay.anand63@gmail.com>
2025-03-04 10:36:42 +0530

    buy sell stock - multiple transactions allowed

---
+++ Arrays/src/BestTimeBuySellStockII.java
@@ -0,0 +1,23 @@
+
+/*
+
+    Problem Statement:
+
+        - Given an integer array 'prices' where prices[i] represent the price of a given stock on the ith day, return
+          the maximum profit one can achieve if multi transactions are allowed.
+
+        - NOTE: You cannot start another transaction in case you already have an ongoing transaction. In simple terms,
+                we must sell before buying again.
+
+        - For example, prices = [7,1,5,3,6,4] -> output = 7 (Buy at 1, Sell at 5; Buy at 3, Sell at 6)
+
+    General Observations:
+
+        - Basic Intuition:
+            - Explore all possible combinations of profitable transactions and find the combination which returns the
+              highest total profit.
+
+*/
+
+public class BestTimeBuySellStockII {
+}
