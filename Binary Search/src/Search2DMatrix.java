152efd9fa4d0ee874960ea8415428ca4f81141d0
nishchayanand <nishchay.anand63@gmail.com>
2025-04-21 07:55:00 +0530

    search a 2D matrix - binary search

--- Binary Search/src/Search2DMatrix.java
+++ Binary Search/src/Search2DMatrix.java
@@ -41,15 +41,52 @@
 
     Optimized Binary Search Approach:
 
-        -
+        - Intuition: Each coordinate in the 2D matrix can be converted to a 1D index using (row * n + col).
 
-        - Time Complexity:
+        - Treat the 2D matrix as a 1D sorted array and run binary search in the range [0, (m * n) - 1] by converting middle
+          element index from 1D index and 2D coordinate using row = mid / n and col = mid % n.
 
-        - Space Complexity:
+        - Algorithm:
+            - left = 0, right = m*n-1;
+            - while left <= right:
+                - mid = (left + right) / 2;
+                - row = mid / n;
+                - col = mid % n;
+                - if matrix[row][col] == target: return true;
+                - if matrix[row][col] > target: right = mid - 1;
+                - if matrix[row][col] < target: left = mid + 1;
+            - return false;
+
+        - Time Complexity: O(log(m*n)).
+
+        - Space Complexity: O(1).
 
 */
 
 public class Search2DMatrix {
 
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m*n-1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) right = mid - 1;
+            else left = mid + 1; // matrix[row][col] < target
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,3,5,7},
+                {10,11,16,20},
+                {23,30,34,60}
+        };
+        int target = 4;
+        System.out.println(searchMatrix(matrix, target));
+    }
 
 }
